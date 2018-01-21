efb64ad2b2cdc308511f4b7fe7d614a013f8d527
Tommy-THINK\Tommy <hoehoetester1@gmail.com>
2018-01-21 23:14:10 +1300

    added debounce demo

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+// background change
 (function () {
 
-    var images = ['./images/image_001.jpg', './images/image_002.jpg', './images/image_003.jpg'];
-    var index = 0;
+    var images = ['./images/image_001.jpg', './images/image_002.jpg', './images/image_003.jpg'],
+        index = 0;
 
     function updateImage() {
         document.body.style.backgroundImage = 'url(' + images[index] + ')';
@@ -12,4 +13,28 @@
     }
 
     setInterval(updateImage, 4000);
+})();
+
+
+// debounce sample
+(function () {
+    function debounce(fn, wait) {
+        var timeout;
+        return function () {
+            var _this = this,
+                args = arguments;
+            clearTimeout(timeout);
+            timeout = setTimeout(function () {
+                fn.apply(_this, args);
+            }, wait || 0);
+        };
+    };
+
+    function click() {
+        console.log('clicked!');
+    }
+
+    document.querySelector('button').addEventListener('click',
+        debounce(click, 500)
+    );
 })();
